92b93d679c7e1c46eb88f697fa13ec70ed8830cb
alex <Alexander3a00009@gmail.com>
2022-10-10 10:02:56 +0200

    added HelloWorld and Program1

---
+++ src/Program1.java
@@ -0,0 +1,6 @@
+public class Program1 {
+    public static void main(String[] args) {
+        System.out.println("This is my first Java program");
+        System.out.println("but it won't be my last.");
+    }
+}
