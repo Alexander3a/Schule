baca91becfc43794712b36bf12e3242619e07996
alex <Alexander3a00009@gmail.com>
2022-10-10 16:07:14 +0200

    shorter but worse fix

--- src/de/alex/Studium/first/A7VorstellungEinzeilig.java
+++ src/de/alex/Studium/first/A7VorstellungEinzeilig.java
@@ -7,15 +7,25 @@ import java.util.Random;
 import static de.alex.Studium.first.globals.*;
 public class A7VorstellungEinzeilig {
     public static void main(String[] args) {
-        System.out.println(do_stuff(new Integer[]{11},""));
-
+        do_stuff(new Integer[]{11});
+        System.out.println();
+        System.out.flush();
     }
-    public static String do_stuff(Integer[] to,String d){
-        if(to[0]==0)return d;
+    public static void do_stuff(Integer[] to){
+        if(to[0]==0)return;
         to[0]--;
-//        System.out.println(namen.get(new Random().nextInt(namen.size())));
-        d+=namen.get(new Random().nextInt(namen.size()))+" ";
-        d= do_stuff(to,d);
-        return d;
+        System.out.print(namen.get(new Random().nextInt(namen.size()))+" ");
+        do_stuff(to);
     }
+//    public static void main(String[] args) {
+//        System.out.println(do_stuff(new Integer[]{11},""));
+//    }
+//    public static String do_stuff(Integer[] to,String d){
+//        if(to[0]==0)return d;
+//        to[0]--;
+////        System.out.println(namen.get(new Random().nextInt(namen.size())));
+//        d+=namen.get(new Random().nextInt(namen.size()))+" ";
+//        d= do_stuff(to,d);
+//        return d;
+//    }
 }
