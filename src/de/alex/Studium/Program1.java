3ab7ef736dc68935d4c4ba6095ff07c7cbc05ccc
alex <Alexander3a00009@gmail.com>
2022-10-10 11:27:02 +0200

    changed args printer and added invert

--- src/de/alex/Studium/Program1.java
+++ src/de/alex/Studium/Program1.java
@@ -3,12 +3,19 @@ package de.alex.Studium;
 public class Program1 {
     public static void main(String[] args) {
         System.out.println("This is my first Java program");
-        System.out.println("but it won't be my last.");
-        StringBuilder a = new StringBuilder();
-        for (String arg : args) {
-            if(!a.toString().equals(""))a.append(" ");
-            a.append(arg);
+        System.out.println( invert("but it won't be my last."));
+        for (int i = 0; i < args.length; i++) {
+            if(i!=0)System.out.print(" ");
+            System.out.print(args[i]);
+            if(i==args.length-1)System.out.print("\n");
         }
-        System.out.println(a);
+        System.out.flush();
+    }
+    public static String invert(String in){
+        StringBuilder a = new StringBuilder(in.length());
+        for (int i = 0; i < in.length(); i++) {
+            a.append(in.charAt(in.length()-i-1));
+        }
+        return a.toString();
     }
 }
